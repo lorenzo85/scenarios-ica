53130cfc670ebc7d4d682bb35a4f59498c4e6639
Lorenzo <gironi.lorenzo@gmail.com>
2025-05-25 09:30:05 +0200

    Fix istio installation

--- security-authentication-mtls/init/background.sh
+++ security-authentication-mtls/init/background.sh
@@ -8,30 +8,3 @@ done
 
 # mark k8s finished
 touch /ks/.k8sfinished
-
-# Install Istio
-export ISTIO_VERSION=1.18.2
-curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
-# Set PATH in .bashrc because no subshell can set parent environment variables
-echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> ~/.bashrc
-export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH # set for istioctl below
-# Istio autocomplete
-echo "[[ -r \"/usr/local/etc/profile.d/bash_completion.sh\" ]] && . \"/usr/local/etc/profile.d/bash_completion.sh\"" >> ~/.bash_profile
-cp /root/istio-${ISTIO_VERSION}/tools/istioctl.bash ./istioctl.bash
-echo "source ~/istioctl.bash" >> ~/.bashrc
-# Deploy Istio with demo profile
-mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
-istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
-touch /ks/.istiofinished
-
-# Mark init finished
-kubectl create namespace foo
-kubectl label namespace foo istio-injection=enabled
-kubectl create namespace bar
-kubectl label namespace bar istio-injection=enabled
-kubectl run tester --image=curlimages/curl:8.6.0 -- sleep 1d
-kubectl run tester --image=curlimages/curl:8.6.0 -n foo -- sleep 1d
-kubectl apply -f /tmp/notification-deployment.yaml
-kubectl apply -f /tmp/booking-deployment.yaml
-
-touch /ks/.initfinished
