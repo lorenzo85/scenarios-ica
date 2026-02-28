f4225ef9ca3250e668629ab7c5259427846ab5bd
Lorenzo <gironi.lorenzo@gmail.com>
2026-02-28 11:48:26 +0100

    Bugfix

--- ingress-kubernetes/step6/verify.sh
+++ ingress-kubernetes/step6/verify.sh
@@ -5,7 +5,7 @@ source /tmp/check-utils.sh
 check_resource_exists ingress booking-ingress
 check_path_value ingress booking-ingress '.spec.ingressClassName' 'istio'
 check_array_length ingress booking-ingress '.spec.rules' 1
-check_path_value ingress booking-ingress '.spec.rules[0].host' 'booking.example.com'
+check_path_host ingress booking-ingress '.spec.rules[0].host' '*.killercoda.com'
 check_array_length ingress booking-ingress '.spec.rules[0].http.paths' 1
 check_path_value ingress booking-ingress '.spec.rules[0].http.paths[0].pathType' 'Prefix'
 check_path_value ingress booking-ingress '.spec.rules[0].http.paths[0].backend.service.name' 'booking-service'
