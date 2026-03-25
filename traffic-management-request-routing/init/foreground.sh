3bdb254f2446e6daada809db7822235758726d9d
Lorenzo <gironi.lorenzo@gmail.com>
2026-03-25 07:44:38 +0100

    Fix Istio/Cilium compatibility

--- traffic-management-request-routing/init/foreground.sh
+++ traffic-management-request-routing/init/foreground.sh
@@ -15,6 +15,9 @@ echo "alias k='kubectl'" >> ~/.bashrc
 source ${HOME}/.bashrc
 # Deploy Istio with demo profile
 mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
+# We need to properly configure Cilium so that it can co-exist with Istio.
+kubectl -n kube-system patch configmap cilium-config --type merge -p '{"data":{"cni-exclusive":"false"}}'
+kubectl -n kube-system rollout restart daemonset cilium
 istioctl install --set profile=demo --set components.cni.enabled=true -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
 # Init scenario
 kubectl label namespace default istio-injection=enabled
