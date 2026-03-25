d48a92ac97c8798ad52f69366c1fe808aa6ccb12
Lorenzo <gironi.lorenzo@gmail.com>
2026-03-25 09:47:01 +0100

    Fix Istio/Cilium compatibility

--- ingress-gateway-no-tsl-termination/init/foreground.sh
+++ ingress-gateway-no-tsl-termination/init/foreground.sh
@@ -15,6 +15,12 @@ echo "alias k='kubectl'" >> ~/.bashrc
 source ${HOME}/.bashrc
 # Deploy Istio with demo profile
 mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
+# We need to properly configure Cilium so that it can co-exist with Istio.
+kubectl -n kube-system patch configmap cilium-config --type merge -p '{"data":{"kubeProxyReplacement":"true"}}'
+kubectl -n kube-system patch configmap cilium-config --type merge -p '{"data":{"socketLB.hostNamespaceOnly":"true"}}'
+kubectl -n kube-system patch configmap cilium-config --type merge -p '{"data":{"cni-exclusive":"false"}}'
+kubectl -n kube-system patch configmap cilium-config --type merge -p '{"data":{"bpf-lb-sock-hostns-only":"true"}}'
+kubectl -n kube-system rollout restart daemonset cilium
 istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
 # Init scenario
 # Create a root example.com certificate and private key to sign the certificates for your services subdomain:
